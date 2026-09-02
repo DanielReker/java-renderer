c110a99106f282b706b52cbf4204ee8f2158e984
DanielReker <sasha29072004@gmail.com>
2026-09-02 15:18:43 +0400

    Fix row/column index order in matrices

--- src/main/java/io/github/danielreker/javarenderer/math/Matrix3f.java
+++ src/main/java/io/github/danielreker/javarenderer/math/Matrix3f.java
@@ -1,9 +1,9 @@
 package io.github.danielreker.javarenderer.math;
 
 public record Matrix3f(
-        float m00, float m10, float m20,
-        float m01, float m11, float m21,
-        float m02, float m12, float m22
+        float m00, float m01, float m02,
+        float m10, float m11, float m12,
+        float m20, float m21, float m22
 ) {
 
     public static final Matrix3f IDENTITY = new Matrix3f(
@@ -12,11 +12,11 @@ public record Matrix3f(
             0.0f, 0.0f, 1.0f
     );
 
-    public static Vector3f multiply(Matrix3f Matrix3f, Vector3f Vector3f) {
+    public static Vector3f multiply(Matrix3f matrix3f, Vector3f vector3f) {
         return new Vector3f(
-                Matrix3f.m00 * Vector3f.v0() + Matrix3f.m10 * Vector3f.v1() + Matrix3f.m20 * Vector3f.v2(),
-                Matrix3f.m01 * Vector3f.v0() + Matrix3f.m11 * Vector3f.v1() + Matrix3f.m21 * Vector3f.v2(),
-                Matrix3f.m02 * Vector3f.v0() + Matrix3f.m12 * Vector3f.v1() + Matrix3f.m22 * Vector3f.v2()
+                matrix3f.m00 * vector3f.v0() + matrix3f.m01 * vector3f.v1() + matrix3f.m02 * vector3f.v2(),
+                matrix3f.m10 * vector3f.v0() + matrix3f.m11 * vector3f.v1() + matrix3f.m12 * vector3f.v2(),
+                matrix3f.m20 * vector3f.v0() + matrix3f.m21 * vector3f.v1() + matrix3f.m22 * vector3f.v2()
         );
     }
 
--- src/main/java/io/github/danielreker/javarenderer/math/Matrix4f.java
+++ src/main/java/io/github/danielreker/javarenderer/math/Matrix4f.java
@@ -5,10 +5,10 @@ import java.util.Optional;
 import static io.github.danielreker.javarenderer.math.Constants.EPSILON_F;
 
 public record Matrix4f(
-        float m00, float m10, float m20, float m30,
-        float m01, float m11, float m21, float m31,
-        float m02, float m12, float m22, float m32,
-        float m03, float m13, float m23, float m33
+        float m00, float m01, float m02, float m03,
+        float m10, float m11, float m12, float m13,
+        float m20, float m21, float m22, float m23,
+        float m30, float m31, float m32, float m33
 ) {
 
     public static final Matrix4f IDENTITY = new Matrix4f(
@@ -21,18 +21,18 @@ public record Matrix4f(
 
     public Matrix3f mat3() {
         return new Matrix3f(
-                m00, m10, m20,
-                m01, m11, m21,
-                m02, m12, m22
+                m00, m01, m02,
+                m10, m11, m12,
+                m20, m21, m22
         );
     }
 
     public Matrix4f transpose() {
         return new Matrix4f(
-                m00, m01, m02, m03,
-                m10, m11, m12, m13,
-                m20, m21, m22, m23,
-                m30, m31, m32, m33
+                m00, m10, m20, m30,
+                m01, m11, m21, m31,
+                m02, m12, m22, m32,
+                m03, m13, m23, m33
         );
     }
 
@@ -78,30 +78,6 @@ public record Matrix4f(
                         + m13 * m21 * m32
                         - m13 * m22 * m31) / det,
 
-                // m10
-                (-m10 * m22 * m33
-                        + m10 * m23 * m32
-                        + m12 * m20 * m33
-                        - m12 * m23 * m30
-                        - m13 * m20 * m32
-                        + m13 * m22 * m30) / det,
-
-                // m20
-                (m10 * m21 * m33
-                        - m10 * m23 * m31
-                        - m11 * m20 * m33
-                        + m11 * m23 * m30
-                        + m13 * m20 * m31
-                        - m13 * m21 * m30) / det,
-
-                // m30
-                (-m10 * m21 * m32
-                        + m10 * m22 * m31
-                        + m11 * m20 * m32
-                        - m11 * m22 * m30
-                        - m12 * m20 * m31
-                        + m12 * m21 * m30) / det,
-
                 // m01
                 (-m01 * m22 * m33
                         + m01 * m23 * m32
@@ -109,30 +85,6 @@ public record Matrix4f(
                         - m02 * m23 * m31
                         - m03 * m21 * m32
                         + m03 * m22 * m31) / det,
-
-                // m11
-                (m00 * m22 * m33
-                        - m00 * m23 * m32
-                        - m02 * m20 * m33
-                        + m02 * m23 * m30
-                        + m03 * m20 * m32
-                        - m03 * m22 * m30) / det,
-
-                // m21
-                (-m00 * m21 * m33
-                        + m00 * m23 * m31
-                        + m01 * m20 * m33
-                        - m01 * m23 * m30
-                        - m03 * m20 * m31
-                        + m03 * m21 * m30) / det,
-
-                // m31
-                (m00 * m21 * m32
-                        - m00 * m22 * m31
-                        - m01 * m20 * m32
-                        + m01 * m22 * m30
-                        + m02 * m20 * m31
-                        - m02 * m21 * m30) / det,
 
                 // m02
                 (m01 * m12 * m33
@@ -142,30 +94,6 @@ public record Matrix4f(
                         + m03 * m11 * m32
                         - m03 * m12 * m31) / det,
 
-                // m12
-                (-m00 * m12 * m33
-                        + m00 * m13 * m32
-                        + m02 * m10 * m33
-                        - m02 * m13 * m30
-                        - m03 * m10 * m32
-                        + m03 * m12 * m30) / det,
-
-                // m22
-                (m00 * m11 * m33
-                        - m00 * m13 * m31
-                        - m01 * m10 * m33
-                        + m01 * m13 * m30
-                        + m03 * m10 * m31
-                        - m03 * m11 * m30) / det,
-
-                // m32
-                (-m00 * m11 * m32
-                        + m00 * m12 * m31
-                        + m01 * m10 * m32
-                        - m01 * m12 * m30
-                        - m02 * m10 * m31
-                        + m02 * m11 * m30) / det,
-
                 // m03
                 (-m01 * m12 * m23
                         + m01 * m13 * m22
@@ -173,6 +101,30 @@ public record Matrix4f(
                         - m02 * m13 * m21
                         - m03 * m11 * m22
                         + m03 * m12 * m21) / det,
+
+                // m10
+                (-m10 * m22 * m33
+                        + m10 * m23 * m32
+                        + m12 * m20 * m33
+                        - m12 * m23 * m30
+                        - m13 * m20 * m32
+                        + m13 * m22 * m30) / det,
+
+                // m11
+                (m00 * m22 * m33
+                        - m00 * m23 * m32
+                        - m02 * m20 * m33
+                        + m02 * m23 * m30
+                        + m03 * m20 * m32
+                        - m03 * m22 * m30) / det,
+
+                // m12
+                (-m00 * m12 * m33
+                        + m00 * m13 * m32
+                        + m02 * m10 * m33
+                        - m02 * m13 * m30
+                        - m03 * m10 * m32
+                        + m03 * m12 * m30) / det,
 
                 // m13
                 (m00 * m12 * m23
@@ -182,6 +134,30 @@ public record Matrix4f(
                         + m03 * m10 * m22
                         - m03 * m12 * m20) / det,
 
+                // m20
+                (m10 * m21 * m33
+                        - m10 * m23 * m31
+                        - m11 * m20 * m33
+                        + m11 * m23 * m30
+                        + m13 * m20 * m31
+                        - m13 * m21 * m30) / det,
+
+                // m21
+                (-m00 * m21 * m33
+                        + m00 * m23 * m31
+                        + m01 * m20 * m33
+                        - m01 * m23 * m30
+                        - m03 * m20 * m31
+                        + m03 * m21 * m30) / det,
+
+                // m22
+                (m00 * m11 * m33
+                        - m00 * m13 * m31
+                        - m01 * m10 * m33
+                        + m01 * m13 * m30
+                        + m03 * m10 * m31
+                        - m03 * m11 * m30) / det,
+
                 // m23
                 (-m00 * m11 * m23
                         + m00 * m13 * m21
@@ -189,6 +165,30 @@ public record Matrix4f(
                         - m01 * m13 * m20
                         - m03 * m10 * m21
                         + m03 * m11 * m20) / det,
+
+                // m30
+                (-m10 * m21 * m32
+                        + m10 * m22 * m31
+                        + m11 * m20 * m32
+                        - m11 * m22 * m30
+                        - m12 * m20 * m31
+                        + m12 * m21 * m30) / det,
+
+                // m31
+                (m00 * m21 * m32
+                        - m00 * m22 * m31
+                        - m01 * m20 * m32
+                        + m01 * m22 * m30
+                        + m02 * m20 * m31
+                        - m02 * m21 * m30) / det,
+
+                // m32
+                (-m00 * m11 * m32
+                        + m00 * m12 * m31
+                        + m01 * m10 * m32
+                        - m01 * m12 * m30
+                        - m02 * m10 * m31
+                        + m02 * m11 * m30) / det,
 
                 // m33
                 (m00 * m11 * m22
@@ -203,34 +203,34 @@ public record Matrix4f(
 
     public static Matrix4f multiply(Matrix4f lhs, Matrix4f rhs) {
         return new Matrix4f(
-                lhs.m00 * rhs.m00 + lhs.m10 * rhs.m01 + lhs.m20 * rhs.m02 + lhs.m30 * rhs.m03,
-                lhs.m00 * rhs.m10 + lhs.m10 * rhs.m11 + lhs.m20 * rhs.m12 + lhs.m30 * rhs.m13,
-                lhs.m00 * rhs.m20 + lhs.m10 * rhs.m21 + lhs.m20 * rhs.m22 + lhs.m30 * rhs.m23,
-                lhs.m00 * rhs.m30 + lhs.m10 * rhs.m31 + lhs.m20 * rhs.m32 + lhs.m30 * rhs.m33,
+                lhs.m00 * rhs.m00 + lhs.m01 * rhs.m10 + lhs.m02 * rhs.m20 + lhs.m03 * rhs.m30,
+                lhs.m00 * rhs.m01 + lhs.m01 * rhs.m11 + lhs.m02 * rhs.m21 + lhs.m03 * rhs.m31,
+                lhs.m00 * rhs.m02 + lhs.m01 * rhs.m12 + lhs.m02 * rhs.m22 + lhs.m03 * rhs.m32,
+                lhs.m00 * rhs.m03 + lhs.m01 * rhs.m13 + lhs.m02 * rhs.m23 + lhs.m03 * rhs.m33,
 
-                lhs.m01 * rhs.m00 + lhs.m11 * rhs.m01 + lhs.m21 * rhs.m02 + lhs.m31 * rhs.m03,
-                lhs.m01 * rhs.m10 + lhs.m11 * rhs.m11 + lhs.m21 * rhs.m12 + lhs.m31 * rhs.m13,
-                lhs.m01 * rhs.m20 + lhs.m11 * rhs.m21 + lhs.m21 * rhs.m22 + lhs.m31 * rhs.m23,
-                lhs.m01 * rhs.m30 + lhs.m11 * rhs.m31 + lhs.m21 * rhs.m32 + lhs.m31 * rhs.m33,
+                lhs.m10 * rhs.m00 + lhs.m11 * rhs.m10 + lhs.m12 * rhs.m20 + lhs.m13 * rhs.m30,
+                lhs.m10 * rhs.m01 + lhs.m11 * rhs.m11 + lhs.m12 * rhs.m21 + lhs.m13 * rhs.m31,
+                lhs.m10 * rhs.m02 + lhs.m11 * rhs.m12 + lhs.m12 * rhs.m22 + lhs.m13 * rhs.m32,
+                lhs.m10 * rhs.m03 + lhs.m11 * rhs.m13 + lhs.m12 * rhs.m23 + lhs.m13 * rhs.m33,
 
-                lhs.m02 * rhs.m00 + lhs.m12 * rhs.m01 + lhs.m22 * rhs.m02 + lhs.m32 * rhs.m03,
-                lhs.m02 * rhs.m10 + lhs.m12 * rhs.m11 + lhs.m22 * rhs.m12 + lhs.m32 * rhs.m13,
-                lhs.m02 * rhs.m20 + lhs.m12 * rhs.m21 + lhs.m22 * rhs.m22 + lhs.m32 * rhs.m23,
-                lhs.m02 * rhs.m30 + lhs.m12 * rhs.m31 + lhs.m22 * rhs.m32 + lhs.m32 * rhs.m33,
+                lhs.m20 * rhs.m00 + lhs.m21 * rhs.m10 + lhs.m22 * rhs.m20 + lhs.m23 * rhs.m30,
+                lhs.m20 * rhs.m01 + lhs.m21 * rhs.m11 + lhs.m22 * rhs.m21 + lhs.m23 * rhs.m31,
+                lhs.m20 * rhs.m02 + lhs.m21 * rhs.m12 + lhs.m22 * rhs.m22 + lhs.m23 * rhs.m32,
+                lhs.m20 * rhs.m03 + lhs.m21 * rhs.m13 + lhs.m22 * rhs.m23 + lhs.m23 * rhs.m33,
 
-                lhs.m03 * rhs.m00 + lhs.m13 * rhs.m01 + lhs.m23 * rhs.m02 + lhs.m33 * rhs.m03,
-                lhs.m03 * rhs.m10 + lhs.m13 * rhs.m11 + lhs.m23 * rhs.m12 + lhs.m33 * rhs.m13,
-                lhs.m03 * rhs.m20 + lhs.m13 * rhs.m21 + lhs.m23 * rhs.m22 + lhs.m33 * rhs.m23,
-                lhs.m03 * rhs.m30 + lhs.m13 * rhs.m31 + lhs.m23 * rhs.m32 + lhs.m33 * rhs.m33
+                lhs.m30 * rhs.m00 + lhs.m31 * rhs.m10 + lhs.m32 * rhs.m20 + lhs.m33 * rhs.m30,
+                lhs.m30 * rhs.m01 + lhs.m31 * rhs.m11 + lhs.m32 * rhs.m21 + lhs.m33 * rhs.m31,
+                lhs.m30 * rhs.m02 + lhs.m31 * rhs.m12 + lhs.m32 * rhs.m22 + lhs.m33 * rhs.m32,
+                lhs.m30 * rhs.m03 + lhs.m31 * rhs.m13 + lhs.m32 * rhs.m23 + lhs.m33 * rhs.m33
         );
     }
 
     public static Vector4f multiply(Matrix4f matrix4f, Vector4f vector4f) {
         return new Vector4f(
-                matrix4f.m00 * vector4f.v0() + matrix4f.m10 * vector4f.v1() + matrix4f.m20 * vector4f.v2() + matrix4f.m30 * vector4f.v3(),
-                matrix4f.m01 * vector4f.v0() + matrix4f.m11 * vector4f.v1() + matrix4f.m21 * vector4f.v2() + matrix4f.m31 * vector4f.v3(),
-                matrix4f.m02 * vector4f.v0() + matrix4f.m12 * vector4f.v1() + matrix4f.m22 * vector4f.v2() + matrix4f.m32 * vector4f.v3(),
-                matrix4f.m03 * vector4f.v0() + matrix4f.m13 * vector4f.v1() + matrix4f.m23 * vector4f.v2() + matrix4f.m33 * vector4f.v3()
+                matrix4f.m00 * vector4f.v0() + matrix4f.m01 * vector4f.v1() + matrix4f.m02 * vector4f.v2() + matrix4f.m03 * vector4f.v3(),
+                matrix4f.m10 * vector4f.v0() + matrix4f.m11 * vector4f.v1() + matrix4f.m12 * vector4f.v2() + matrix4f.m13 * vector4f.v3(),
+                matrix4f.m20 * vector4f.v0() + matrix4f.m21 * vector4f.v1() + matrix4f.m22 * vector4f.v2() + matrix4f.m23 * vector4f.v3(),
+                matrix4f.m30 * vector4f.v0() + matrix4f.m31 * vector4f.v1() + matrix4f.m32 * vector4f.v2() + matrix4f.m33 * vector4f.v3()
         );
     }
 
